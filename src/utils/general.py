eabce705889bec5923a55d342db87c35cee1a167
Ilmari Vikström <ilmari.vikstrom@tietoevry.com>
2022-11-21 17:31:41 +0200

    Minor UI touches.

--- src/utils/general.py
+++ src/utils/general.py
@@ -17,6 +17,13 @@ def add_bg_from_local(image_file):
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
         background-size: cover
     }}
+    [data-testid="stHeader"] {{
+        background-color: rgba(0, 0, 0, 0);
+    }}
+    [data-testid="stSidebar"] {{
+        background: #232526;
+        background: linear-gradient(to up, #414345, #232526);
+    }}
     img {{
         border-radius: 15px;
     }}
